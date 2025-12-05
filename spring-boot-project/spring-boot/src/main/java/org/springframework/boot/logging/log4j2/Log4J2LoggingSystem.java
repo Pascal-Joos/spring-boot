a06bffaf8d8aca651a008797d9523f5505a3cd15
Pascal-Joos <pascal.joos@outlook.de>
2025-12-05 20:36:32 +0000

    fix: 54 - DEREFERENCE_NULLABLE - logger.setLevel(null); - dereferenced expression logger is @Nullable

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/logging/log4j2/Log4J2LoggingSystem.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/logging/log4j2/Log4J2LoggingSystem.java
@@ -373,13 +373,13 @@ public class Log4J2LoggingSystem extends AbstractLoggingSystem {
 	}
 
 	private void clearLogLevel(@Nullable String loggerName, @Nullable LoggerConfig logger) {
-		if (logger instanceof LevelSetLoggerConfig) {
-			getLoggerContext().getConfiguration().removeLogger(loggerName);
+			if (logger == null || logger instanceof LevelSetLoggerConfig) {
+				getLoggerContext().getConfiguration().removeLogger(loggerName);
+			}
+			else {
+				logger.setLevel(null);
+			}
 		}
-		else {
-			logger.setLevel(null);
-		}
-	}
 
 	private void setLogLevel(@Nullable String loggerName, @Nullable LoggerConfig logger, Level level) {
 		if (logger == null) {
