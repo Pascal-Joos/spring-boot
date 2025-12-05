c3dc4173886f912fe9993b37586a3e14be65fa48
Pascal-Joos <pascal.joos@outlook.de>
2025-12-05 22:35:26 +0000

    fix: 78 - DEREFERENCE_NULLABLE - for (AnnotationAttributes initParam : (AnnotationAttributes[]) attributes.get("initParams")) { - enhanced-for expression (AnnotationAttributes[]) attributes.get("initParams") is @Nullable

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/web/servlet/ServletComponentHandler.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/web/servlet/ServletComponentHandler.java
@@ -60,15 +60,18 @@ abstract class ServletComponentHandler {
 		return value;
 	}
 
-	protected final Map<String, String> extractInitParameters(Map<String, Object> attributes) {
-		Map<String, String> initParameters = new HashMap<>();
-		for (AnnotationAttributes initParam : (AnnotationAttributes[]) attributes.get("initParams")) {
-			String name = (String) initParam.get("name");
-			String value = (String) initParam.get("value");
-			initParameters.put(name, value);
+		protected final Map<String, String> extractInitParameters(Map<String, Object> attributes) {
+			Map<String, String> initParameters = new HashMap<>();
+			AnnotationAttributes[] initParams = (AnnotationAttributes[]) attributes.get("initParams");
+			if (initParams != null) {
+				for (AnnotationAttributes initParam : initParams) {
+					String name = (String) initParam.get("name");
+					String value = (String) initParam.get("value");
+					initParameters.put(name, value);
+				}
+			}
+			return initParameters;
 		}
-		return initParameters;
-	}
 
 	void handle(AnnotatedBeanDefinition beanDefinition, BeanDefinitionRegistry registry) {
 		Map<String, Object> attributes = beanDefinition.getMetadata()
