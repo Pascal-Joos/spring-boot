11ed1225271f2628e2b44055d030a7205dde6f57
Pascal-Joos <pascal.joos@outlook.de>
2025-12-05 17:01:36 +0000

    fix: 9 - DEREFERENCE_NULLABLE - this.logger.trace(LogMessage.format("Setting default profiles: %s", profiles.getDefault())); - dereferenced expression profiles is @Nullable

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/context/config/ConfigDataEnvironment.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/context/config/ConfigDataEnvironment.java
@@ -330,12 +330,14 @@ class ConfigDataEnvironment {
 		MutablePropertySources propertySources = this.environment.getPropertySources();
 		applyContributor(contributors, activationContext, propertySources);
 		DefaultPropertiesPropertySource.moveToEnd(propertySources);
-		Profiles profiles = activationContext.getProfiles();
-		this.logger.trace(LogMessage.format("Setting default profiles: %s", profiles.getDefault()));
-		this.environment.setDefaultProfiles(StringUtils.toStringArray(profiles.getDefault()));
-		this.logger.trace(LogMessage.format("Setting active profiles: %s", profiles.getActive()));
-		this.environment.setActiveProfiles(StringUtils.toStringArray(profiles.getActive()));
-		this.environmentUpdateListener.onSetProfiles(profiles);
+			Profiles profiles = activationContext.getProfiles();
+			if (profiles != null) {
+				this.logger.trace(LogMessage.format("Setting default profiles: %s", profiles.getDefault()));
+				this.environment.setDefaultProfiles(StringUtils.toStringArray(profiles.getDefault()));
+				this.logger.trace(LogMessage.format("Setting active profiles: %s", profiles.getActive()));
+				this.environment.setActiveProfiles(StringUtils.toStringArray(profiles.getActive()));
+				this.environmentUpdateListener.onSetProfiles(profiles);
+			}
 	}
 
 	private void applyContributor(ConfigDataEnvironmentContributors contributors,
