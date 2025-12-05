6e23bb21b9f93fb162a881aea648b7232e32632a
Pascal-Joos <pascal.joos@outlook.de>
2025-12-05 21:12:55 +0000

    Revert "fix: 61 - DEREFERENCE_NULLABLE - return instantiate(names.stream().map((name) -> TypeSupplier.forName(classLoader, name))); - dereferenced expression names is @Nullable"
    This reverts commit 7e2b8594b4646f362b83b42e48fcc522c8e822a1.

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/util/Instantiator.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/util/Instantiator.java
@@ -123,11 +123,9 @@ public class Instantiator<T> {
 	 * @since 2.4.8
 	 */
 	public List<T> instantiate(@Nullable ClassLoader classLoader, @Nullable Collection<String> names) {
-			if (names == null) {
-				return Collections.emptyList();
-			}
-			return instantiate(names.stream().map((name) -> TypeSupplier.forName(classLoader, name)));
-		}
+		Assert.notNull(names, "Names must not be null");
+		return instantiate(names.stream().map((name) -> TypeSupplier.forName(classLoader, name)));
+	}
 
 	/**
 	 * Instantiate the given set of classes, injecting constructor arguments as necessary.
@@ -173,7 +171,6 @@ public class Instantiator<T> {
 		throw new IllegalAccessException("Class [" + type.getName() + "] has no suitable constructor");
 	}
 
-		@Nullable
 	private Object[] getArgs(Class<?>[] parameterTypes) {
 		Object[] args = new Object[parameterTypes.length];
 		for (int i = 0; i < parameterTypes.length; i++) {
