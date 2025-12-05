a0325fd470ab2d79f2832bfe934b0ba1b51d4494
Pascal-Joos <pascal.joos@outlook.de>
2025-12-05 22:17:04 +0000

    fix: 74 - DEREFERENCE_NULLABLE - if (bindException.getMessage().toLowerCase().contains("in use")) { - dereferenced expression bindException.getMessage() is @Nullable

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/web/server/PortInUseException.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/web/server/PortInUseException.java
@@ -82,7 +82,8 @@ public class PortInUseException extends WebServerException {
 	public static void ifPortBindingException(Exception ex, Consumer<BindException> action) {
 		ifCausedBy(ex, BindException.class, (bindException) -> {
 			// bind exception can be also thrown because an address can't be assigned
-			if (bindException.getMessage().toLowerCase().contains("in use")) {
+				String message = bindException.getMessage();
+				if (message != null && message.toLowerCase().contains("in use")) {
 				action.accept(bindException);
 			}
 		});
