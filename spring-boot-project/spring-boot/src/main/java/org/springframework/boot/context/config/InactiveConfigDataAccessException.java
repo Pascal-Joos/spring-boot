dc64f19533c7d980a9dfd014f9aad3ad2124504f
Pascal-Joos <pascal.joos@outlook.de>
2025-12-05 17:22:45 +0000

    Revert "fix: 13 - DEREFERENCE_NULLABLE - message.append(propertySource.getName()); - dereferenced expression propertySource is @Nullable"
    This reverts commit 58fa554f9f997fba4e4d1cea21f102b44b69589c.

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/context/config/InactiveConfigDataAccessException.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/context/config/InactiveConfigDataAccessException.java
@@ -65,7 +65,7 @@ public class InactiveConfigDataAccessException extends ConfigDataException {
 	private static String getMessage(@Nullable PropertySource<?> propertySource, @Nullable ConfigDataResource location,
 			String propertyName, @Nullable Origin origin) {
 		StringBuilder message = new StringBuilder("Inactive property source '");
-		message.append((propertySource != null) ? propertySource.getName() : "unknown");
+		message.append(propertySource.getName());
 		if (location != null) {
 			message.append("' imported from location '");
 			message.append(location);
