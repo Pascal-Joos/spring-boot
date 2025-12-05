1cda85270d1216dcd6f59ec55ffacef5eacc95c8
Pascal-Joos <pascal.joos@outlook.de>
2025-12-05 20:01:52 +0000

    fix: 47 - PASS_NULLABLE - return convertToString(this.getter.get(dataSource)); - passing @Nullable parameter 'dataSource' where @NonNull is required

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/jdbc/DataSourceBuilder.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/jdbc/DataSourceBuilder.java
@@ -506,33 +506,34 @@ public final class DataSourceBuilder<T extends DataSource> {
 			}
 		}
 
-		@Nullable
-		String get(@Nullable T dataSource) {
-			try {
-				if (this.getter == null) {
-					UnsupportedDataSourcePropertyException.throwIf(!this.property.isOptional(),
-							() -> "No getter mapped for '" + this.property + "' property");
-					return null;
+			@Nullable
+			String get(@Nullable T dataSource) {
+				try {
+					if (this.getter == null) {
+						UnsupportedDataSourcePropertyException.throwIf(!this.property.isOptional(),
+								() -> "No getter mapped for '" + this.property + "' property");
+						return null;
+					}
+					if (dataSource == null) {
+						return null;
+					}
+					return convertToString(this.getter.get(dataSource));
 				}
-				return convertToString(this.getter.get(dataSource));
+				catch (SQLException ex) {
+					throw new IllegalStateException(ex);
+				}
 			}
-			catch (SQLException ex) {
-				throw new IllegalStateException(ex);
+			@Nullable
+			@SuppressWarnings("unchecked")
+			private V convertFromString(@Nullable String value) {
+				if (String.class.equals(this.type)) {
+					return (V) value;
+				}
+				if (Class.class.equals(this.type)) {
+					return (V) ClassUtils.resolveClassName(value, null);
+				}
+				throw new IllegalStateException("Unsupported value type " + this.type);
 			}
-		}
-
-		@Nullable
-		@SuppressWarnings("unchecked")
-		private V convertFromString(@Nullable String value) {
-			if (String.class.equals(this.type)) {
-				return (V) value;
-			}
-			if (Class.class.equals(this.type)) {
-				return (V) ClassUtils.resolveClassName(value, null);
-			}
-			throw new IllegalStateException("Unsupported value type " + this.type);
-		}
-
 		private String convertToString(V value) {
 			if (String.class.equals(this.type)) {
 				return (String) value;
