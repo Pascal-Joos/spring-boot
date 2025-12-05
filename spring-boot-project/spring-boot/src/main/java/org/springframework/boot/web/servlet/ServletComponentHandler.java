11562c9bf4bd7dd26fad640075676342614205bb
Pascal-Joos <pascal.joos@outlook.de>
2025-12-05 22:45:16 +0000

    Revert "fix: 80 - DEREFERENCE_NULLABLE - Assert.state(value.length == 0, "The urlPatterns and value attributes are mutually exclusive."); - dereferenced expression value is @Nullable"
    This reverts commit aed318d604768ce2a1cc99f89034962b1f02d72c.

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/web/servlet/ServletComponentHandler.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/web/servlet/ServletComponentHandler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2020 the original author or authors.
+ * Copyright 2012-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,18 +20,19 @@ import java.lang.annotation.Annotation;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.AnnotatedBeanDefinition;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
-import org.springframework.context.annotation.AnnotatedBeanDefinition;
 import org.springframework.core.annotation.AnnotationAttributes;
 import org.springframework.core.type.filter.AnnotationTypeFilter;
 import org.springframework.core.type.filter.TypeFilter;
-import org.springframework.lang.Nullable;
 import org.springframework.util.Assert;
+import javax.annotation.Nullable;
 
 /**
- * Base class for handlers dealing with servlet components.
+ * Abstract base class for handlers of Servlet components discovered via classpath
+ * scanning.
  *
- * @author Phillip Webb
+ * @author Andy Wilkinson
  */
 abstract class ServletComponentHandler {
 
@@ -52,9 +53,8 @@ abstract class ServletComponentHandler {
 	protected String[] extractUrlPatterns(Map<String, Object> attributes) {
 		String[] value = (String[]) attributes.get("value");
 		String[] urlPatterns = (String[]) attributes.get("urlPatterns");
-		if (urlPatterns != null && urlPatterns.length > 0) {
-			Assert.state(value == null || value.length == 0,
-					"The urlPatterns and value attributes are mutually exclusive.");
+		if (urlPatterns.length > 0) {
+			Assert.state(value.length == 0, "The urlPatterns and value attributes are mutually exclusive.");
 			return urlPatterns;
 		}
 		return value;
@@ -62,13 +62,10 @@ abstract class ServletComponentHandler {
 
 	protected final Map<String, String> extractInitParameters(Map<String, Object> attributes) {
 		Map<String, String> initParameters = new HashMap<>();
-		AnnotationAttributes[] initParams = (AnnotationAttributes[]) attributes.get("initParams");
-		if (initParams != null) {
-			for (AnnotationAttributes initParam : initParams) {
-				String name = (String) initParam.get("name");
-				String value = (String) initParam.get("value");
-				initParameters.put(name, value);
-			}
+		for (AnnotationAttributes initParam : (AnnotationAttributes[]) attributes.get("initParams")) {
+			String name = (String) initParam.get("name");
+			String value = (String) initParam.get("value");
+			initParameters.put(name, value);
 		}
 		return initParameters;
 	}
