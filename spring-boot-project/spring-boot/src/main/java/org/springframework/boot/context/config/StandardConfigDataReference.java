234326997eadc4833be91a8eb989936c84559fd9
Pascal-Joos <pascal.joos@outlook.de>
2025-12-05 17:27:18 +0000

    Revert "fix: 14 - RETURN_NULLABLE - return this.directory; - returning @Nullable expression from method with @NonNull return type"
    This reverts commit 5e1126f8bc94d91a9e830c8de596fa9b07284824.

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/context/config/StandardConfigDataReference.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/context/config/StandardConfigDataReference.java
@@ -73,8 +73,7 @@ class StandardConfigDataReference {
 		return !this.configDataLocation.isOptional() && this.directory != null;
 	}
 
-	@Nullable
-		String getDirectory() {
+	String getDirectory() {
 		return this.directory;
 	}
 
