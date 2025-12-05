5b20fe5fe9c7b1e246c8118b9950927d20ccd9df
Pascal-Joos <pascal.joos@outlook.de>
2025-12-05 16:30:28 +0000

    fix: 4 - DEREFERENCE_NULLABLE - context.setApplicationStartup(this.applicationStartup); - dereferenced expression context is @Nullable

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/SpringApplication.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/SpringApplication.java
@@ -311,6 +311,9 @@ public class SpringApplication {
 			ConfigurableEnvironment environment = prepareEnvironment(listeners, bootstrapContext, applicationArguments);
 			Banner printedBanner = printBanner(environment);
 			context = createApplicationContext();
+			if (context == null) {
+				throw new IllegalStateException("ApplicationContext must not be null");
+			}
 			context.setApplicationStartup(this.applicationStartup);
 			prepareContext(bootstrapContext, context, environment, listeners, applicationArguments, printedBanner);
 			refreshContext(context);
