365f747ac400eb5309f722e15deb222cb5c17d27
Pascal-Joos <pascal.joos@outlook.de>
2025-12-05 22:11:04 +0000

    fix: 73 - DEREFERENCE_NULLABLE - this.beanFactory.getBeansOfType(ErrorPageRegistrar.class, false, false).values()); - dereferenced expression this.beanFactory is @Nullable

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/web/server/ErrorPageRegistrarBeanPostProcessor.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/web/server/ErrorPageRegistrarBeanPostProcessor.java
@@ -75,8 +75,10 @@ public class ErrorPageRegistrarBeanPostProcessor implements BeanPostProcessor, B
 	private Collection<ErrorPageRegistrar> getRegistrars() {
 		if (this.registrars == null) {
 			// Look up does not include the parent context
+			Assert.notNull(this.beanFactory, "beanFactory must not be null");
+			ListableBeanFactory beanFactory = this.beanFactory;
 			this.registrars = new ArrayList<>(
-					this.beanFactory.getBeansOfType(ErrorPageRegistrar.class, false, false).values());
+					beanFactory.getBeansOfType(ErrorPageRegistrar.class, false, false).values());
 			this.registrars.sort(AnnotationAwareOrderComparator.INSTANCE);
 			this.registrars = Collections.unmodifiableList(this.registrars);
 		}
