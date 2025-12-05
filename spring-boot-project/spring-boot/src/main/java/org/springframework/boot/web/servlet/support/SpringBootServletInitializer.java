ee01fb736e86aa7be388042874fc66de43596674
Pascal-Joos <pascal.joos@outlook.de>
2025-12-05 23:10:17 +0000

    Revert "fix: 83 - DEREFERENCE_NULLABLE - this.logger.info("Root context already created (using as parent)."); - dereferenced expression this.logger is @Nullable"
    This reverts commit b047dbb0025985c1abc629e0a2d87cdf991cda39.

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/web/servlet/support/SpringBootServletInitializer.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/web/servlet/support/SpringBootServletInitializer.java
@@ -132,7 +132,7 @@ public abstract class SpringBootServletInitializer implements WebApplicationInit
 		builder.main(getClass());
 		ApplicationContext parent = getExistingRootWebApplicationContext(servletContext);
 		if (parent != null) {
-			if (this.logger != null) { this.logger.info("Root context already created (using as parent)."); }
+			this.logger.info("Root context already created (using as parent).");
 			servletContext.setAttribute(WebApplicationContext.ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE, null);
 			builder.initializers(new ParentContextApplicationContextInitializer(parent));
 		}
