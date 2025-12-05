805dbf263fbdaef4f349f203df87dee9743ac04a
Pascal-Joos <pascal.joos@outlook.de>
2025-12-05 18:45:12 +0000

    fix: 30 - RETURN_NULLABLE - return (result != null && result.getParameterCount() > 0) ? result : null; - returning @Nullable expression from method with @NonNull return type

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/context/properties/bind/DefaultBindConstructorProvider.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/context/properties/bind/DefaultBindConstructorProvider.java
@@ -131,6 +131,7 @@ class DefaultBindConstructorProvider implements BindConstructorProvider {
 			return false;
 		}
 
+ 			@Nullable
 		private static Constructor<?> getConstructorBindingAnnotated(Class<?> type, Constructor<?>[] candidates,
 				MergedAnnotations[] mergedAnnotations) {
 			Constructor<?> result = null;
@@ -146,6 +147,7 @@ class DefaultBindConstructorProvider implements BindConstructorProvider {
 			return result;
 
 		}
+ 			@Nullable
 
 		private static Constructor<?> deduceBindConstructor(Class<?> type, Constructor<?>[] candidates) {
 			if (candidates.length == 1 && candidates[0].getParameterCount() > 0) {
