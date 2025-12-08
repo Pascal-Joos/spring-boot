e7398051f656228860ceb9fdcf38ecaed47f37fa
Pascal-Joos <pascal.joos@outlook.de>
2025-12-08 21:02:04 +0000

    fix: Type='DEREFERENCE_NULLABLE', message='dereferenced expression this.beanFactory is @Nullable' /home/vscode/nullness-benchmarks/spring-boot/spring-boot-project/spring-boot/src/main/java/org/springframework/boot/web/server/WebServerFactoryCustomizerBeanPostProcessor.java:90 		return (Collection) this.beanFactory.getBeansOfType(WebServerFactoryCustomizer.class, false, false).values();

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/web/server/WebServerFactoryCustomizerBeanPostProcessor.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/web/server/WebServerFactoryCustomizerBeanPostProcessor.java
@@ -87,7 +87,9 @@ public class WebServerFactoryCustomizerBeanPostProcessor implements BeanPostProc
 
 	@SuppressWarnings({ "unchecked", "rawtypes" })
 	private Collection<WebServerFactoryCustomizer<?>> getWebServerFactoryCustomizerBeans() {
-		return (Collection) this.beanFactory.getBeansOfType(WebServerFactoryCustomizer.class, false, false).values();
+		ListableBeanFactory beanFactory = java.util.Objects.requireNonNull(this.beanFactory, "BeanFactory must not be null");
+		Assert.state(beanFactory != null, "BeanFactory must not be null");
+		return (Collection) beanFactory.getBeansOfType(WebServerFactoryCustomizer.class, false, false).values();
 	}
 
 }
