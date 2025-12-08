05d568e06127a107ccd85e0c2689ae7108168894
Pascal-Joos <pascal.joos@outlook.de>
2025-12-08 17:39:17 +0000

    fix: Type='DEREFERENCE_NULLABLE', message='dereferenced expression this.bindConstructor is @Nullable' /home/vscode/nullness-benchmarks/spring-boot/spring-boot-project/spring-boot/src/main/java/org/springframework/boot/context/properties/bind/BindableRuntimeHintsRegistrar.java:154 			for (int i = 0; i < this.bindConstructor.getParameterCount(); i++) {

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/context/properties/bind/BindableRuntimeHintsRegistrar.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/context/properties/bind/BindableRuntimeHintsRegistrar.java
@@ -151,6 +151,9 @@ public class BindableRuntimeHintsRegistrar implements RuntimeHintsRegistrar {
 		}
 
 		private void handleValueObjectProperties(ReflectionHints hints) {
+			if (this.bindConstructor == null) {
+				return;
+			}
 			for (int i = 0; i < this.bindConstructor.getParameterCount(); i++) {
 				String propertyName = this.bindConstructor.getParameters()[i].getName();
 				ResolvableType propertyType = ResolvableType.forConstructorParameter(this.bindConstructor, i);
