bb5bb3aa051b14291c8b0d9315ac758e00343b95
Pascal-Joos <pascal.joos@outlook.de>
2025-12-08 20:54:48 +0000

    fix: Type='DEREFERENCE_NULLABLE', message='dereferenced expression bindException.getMessage() is @Nullable' /home/vscode/nullness-benchmarks/spring-boot/spring-boot-project/spring-boot/src/main/java/org/springframework/boot/web/server/PortInUseException.java:85 			if (bindException.getMessage().toLowerCase().contains("in use")) {

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/web/server/PortInUseException.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/web/server/PortInUseException.java
@@ -1,24 +1,10 @@
-/*
- * Copyright 2012-2020 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package org.springframework.boot.web.server;
 
 import java.net.BindException;
+import java.util.Objects;
 import java.util.function.Consumer;
 import java.util.function.IntSupplier;
+
 import javax.annotation.Nullable;
 
 /**
@@ -82,7 +68,7 @@ public class PortInUseException extends WebServerException {
 	public static void ifPortBindingException(Exception ex, Consumer<BindException> action) {
 		ifCausedBy(ex, BindException.class, (bindException) -> {
 			// bind exception can be also thrown because an address can't be assigned
-			if (bindException.getMessage().toLowerCase().contains("in use")) {
+			if (Objects.toString(bindException.getMessage(), "").toLowerCase().contains("in use")) {
 				action.accept(bindException);
 			}
 		});
