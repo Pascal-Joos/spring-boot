88b94393077b7d6aa38f084b660ff5d8ace890ce
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 19:05:49 +0000

    Revert "fix: 42 - DEREFERENCE_NULLABLE - return this.intValue.apply(value); - dereferenced expression this.intValue is @Nullable"
    This reverts commit 0b7abcdab2144d2d09b6dade32a4a1515a1f67df.

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/convert/PeriodStyle.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/convert/PeriodStyle.java
@@ -266,10 +266,9 @@ public enum PeriodStyle {
 		}
 
 		private int intValue(Period value) {
-  			java.util.function.Function<Period, Integer> localIntValue = this.intValue;
-  			Assert.notNull(localIntValue, () -> "intValue cannot be extracted from " + this.name());
-  			return localIntValue.apply(value);
-  		}
+			Assert.notNull(this.intValue, () -> "intValue cannot be extracted from " + this.name());
+			return this.intValue.apply(value);
+		}
 
 		private static Unit fromChronoUnit(@Nullable ChronoUnit chronoUnit) {
 			if (chronoUnit == null) {
