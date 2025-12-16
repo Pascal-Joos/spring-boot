99fe88b94380e81621c61866518caf79f16e79f3
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 19:39:42 +0000

    fix: 53 - ASSIGN_FIELD_NULLABLE - this.destinationSupplier = null; - assigning @Nullable expression to @NonNull field

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/logging/DeferredLog.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/logging/DeferredLog.java
@@ -46,10 +46,12 @@ public class DeferredLog implements Log {
 	/**
 	 * Create a new {@link DeferredLog} instance.
 	 */
-	public DeferredLog() {
-		this.destinationSupplier = null;
-		this.lines = new Lines();
-	}
+	/**
+ 	 * Create a new {@link DeferredLog} instance.
+ 	 */
+ 	public DeferredLog() {
+ 		this(destination -> destination, new Lines());
+ 	}
 
 	/**
 	 * Create a new {@link DeferredLog} instance managed by a {@link DeferredLogFactory}.
