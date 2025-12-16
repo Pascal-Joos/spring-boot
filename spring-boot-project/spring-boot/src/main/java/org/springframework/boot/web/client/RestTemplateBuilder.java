78e03419d00a2650dbcea9477696a7deed966f44
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 20:12:45 +0000

    Revert "fix: 63 - DEREFERENCE_NULLABLE - int timeout = Math.toIntExact(this.readTimeout.toMillis()); - dereferenced expression this.readTimeout is @Nullable"
    This reverts commit 69059b15ab77eac45ae1c640278ab3e7281c15b6.

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/web/client/RestTemplateBuilder.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/web/client/RestTemplateBuilder.java
@@ -784,13 +784,10 @@ public class RestTemplateBuilder {
 		}
 
 		private void setReadTimeout(ClientHttpRequestFactory factory) {
-  			if (this.readTimeout == null) {
-  				return;
-  			}
-  			Method method = findMethod(factory, "setReadTimeout", int.class);
-  			int timeout = Math.toIntExact(this.readTimeout.toMillis());
-  			invoke(factory, method, timeout);
-  		}
+			Method method = findMethod(factory, "setReadTimeout", int.class);
+			int timeout = Math.toIntExact(this.readTimeout.toMillis());
+			invoke(factory, method, timeout);
+		}
 
 		private void setBufferRequestBody(ClientHttpRequestFactory factory) {
 			Method method = findMethod(factory, "setBufferRequestBody", boolean.class);
