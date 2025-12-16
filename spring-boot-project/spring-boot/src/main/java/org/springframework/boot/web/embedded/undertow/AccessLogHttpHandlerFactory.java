3c932057d8c1d866d85a8c8404ab19fda458c3b2
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 20:20:31 +0000

    Revert "fix: 66 - DEREFERENCE_NULLABLE - if (!this.directory.isDirectory() && !this.directory.mkdirs()) { - dereferenced expression this.directory is @Nullable"
    This reverts commit c19c5baba499ef21c60d0da6a627161bb2391a13.

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/web/embedded/undertow/AccessLogHttpHandlerFactory.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/web/embedded/undertow/AccessLogHttpHandlerFactory.java
@@ -80,12 +80,11 @@ class AccessLogHttpHandlerFactory implements HttpHandlerFactory {
 	}
 
 	private void createAccessLogDirectoryIfNecessary() {
- 		File directory = this.directory;
- 		Assert.state(directory != null, "Access log directory is not set");
- 		if (!directory.isDirectory() && !directory.mkdirs()) {
- 			throw new IllegalStateException("Failed to create access log directory '" + directory + "'");
- 		}
- 	}
+		Assert.state(this.directory != null, "Access log directory is not set");
+		if (!this.directory.isDirectory() && !this.directory.mkdirs()) {
+			throw new IllegalStateException("Failed to create access log directory '" + this.directory + "'");
+		}
+	}
 
 	private XnioWorker createWorker() throws IOException {
 		Xnio xnio = Xnio.getInstance(Undertow.class.getClassLoader());
