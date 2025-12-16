3199fe26defa7bfbb1617ab189fa8e8396ebe1b3
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 18:30:14 +0000

    Revert "fix: 28 - RETURN_NULLABLE - return null; - returning @Nullable expression from method with @NonNull return type"
    This reverts commit e94eab81ea145ed844870a99016ede7926d30991.

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/context/properties/bind/DefaultBindConstructorProvider.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/context/properties/bind/DefaultBindConstructorProvider.java
@@ -148,26 +148,23 @@ class DefaultBindConstructorProvider implements BindConstructorProvider {
 		}
 
 		private static Constructor<?> deduceBindConstructor(Class<?> type, Constructor<?>[] candidates) {
-  			if (candidates.length == 1 && candidates[0].getParameterCount() > 0) {
-  				if (type.isMemberClass() && Modifier.isPrivate(candidates[0].getModifiers())) {
-  					throw new IllegalStateException(type.getName() + " has no suitable bind constructor");
-  				}
-  				return candidates[0];
-  			}
-  			Constructor<?> result = null;
-  			for (Constructor<?> candidate : candidates) {
-  				if (!Modifier.isPrivate(candidate.getModifiers())) {
-  					if (result != null) {
-  						throw new IllegalStateException(type.getName() + " has no suitable bind constructor");
-  					}
-  					result = candidate;
-  				}
-  			}
-  			if (result != null && result.getParameterCount() > 0) {
-  				return result;
-  			}
-  			throw new IllegalStateException(type.getName() + " has no suitable bind constructor");
-  		}
+			if (candidates.length == 1 && candidates[0].getParameterCount() > 0) {
+				if (type.isMemberClass() && Modifier.isPrivate(candidates[0].getModifiers())) {
+					return null;
+				}
+				return candidates[0];
+			}
+			Constructor<?> result = null;
+			for (Constructor<?> candidate : candidates) {
+				if (!Modifier.isPrivate(candidate.getModifiers())) {
+					if (result != null) {
+						return null;
+					}
+					result = candidate;
+				}
+			}
+			return (result != null && result.getParameterCount() > 0) ? result : null;
+		}
 
 		private static boolean isKotlinType(Class<?> type) {
 			return KotlinDetector.isKotlinPresent() && KotlinDetector.isKotlinType(type);
