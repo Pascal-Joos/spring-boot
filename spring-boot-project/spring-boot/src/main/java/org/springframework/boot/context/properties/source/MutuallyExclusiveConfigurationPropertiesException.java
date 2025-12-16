dc562ab929f56457d6942237049b46cb2e0d066d
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 19:00:59 +0000

    fix: 39 - RETURN_NULLABLE - return (collection != null) ? new LinkedHashSet<>(collection) : null; - returning @Nullable expression from method with @NonNull return type

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/context/properties/source/MutuallyExclusiveConfigurationPropertiesException.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/context/properties/source/MutuallyExclusiveConfigurationPropertiesException.java
@@ -78,8 +78,8 @@ public class MutuallyExclusiveConfigurationPropertiesException extends RuntimeEx
 	}
 
 	private static Set<String> asSet(Collection<String> collection) {
-		return (collection != null) ? new LinkedHashSet<>(collection) : null;
-	}
+ 		return new LinkedHashSet<>((collection != null) ? collection : Collections.emptyList());
+ 	}
 
 	private static String buildMessage(Set<String> mutuallyExclusiveNames, Set<String> configuredNames) {
 		Assert.isTrue(configuredNames != null && configuredNames.size() > 1,
