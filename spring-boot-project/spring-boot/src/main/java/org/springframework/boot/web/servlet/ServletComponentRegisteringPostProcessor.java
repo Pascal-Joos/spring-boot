8471ea310aa8af799ea469c9c501cf3ef64f7165
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 21:05:50 +0000

    fix: 81 - METHOD_NO_INIT - ServletComponentRegisteringPostProcessor(Set<String> packagesToScan) { - initializer method does not guarantee @NonNull field applicationContext (line 58) is initialized along all control-flow paths (remember to check for exceptions or early returns).

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/web/servlet/ServletComponentRegisteringPostProcessor.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/web/servlet/ServletComponentRegisteringPostProcessor.java
@@ -58,8 +58,11 @@ class ServletComponentRegisteringPostProcessor implements BeanFactoryPostProcess
 	private ApplicationContext applicationContext;
 
 	ServletComponentRegisteringPostProcessor(Set<String> packagesToScan) {
-		this.packagesToScan = packagesToScan;
-	}
+ 		if (packagesToScan == null) {
+ 			throw new IllegalArgumentException("packagesToScan must not be null");
+ 		}
+ 		this.packagesToScan = packagesToScan;
+ 	}
 
 	@Override
 	public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
