63d4b28eebb515457ea7f4355c963ade7e251fa0
Pascal-Joos <pascal.joos@outlook.de>
2025-12-17 13:44:17 +0000

    fix: Type='METHOD_NO_INIT', message='initializer method does not guarantee @NonNull fields ssl (line 52), compression (line 60) are initialized along all control-flow paths (remember to check for exceptions or early returns).' /home/vscode/nullness-benchmarks/spring-boot/spring-boot-project/spring-boot/src/main/java/org/springframework/boot/web/server/AbstractConfigurableWebServerFactory.java:78 	public AbstractConfigurableWebServerFactory(int port) {

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/web/server/AbstractConfigurableWebServerFactory.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/web/server/AbstractConfigurableWebServerFactory.java
@@ -78,8 +78,10 @@ public abstract class AbstractConfigurableWebServerFactory implements Configurab
 	 * @param port the port number for the web server
 	 */
 	public AbstractConfigurableWebServerFactory(int port) {
-		this.port = port;
-	}
+ 		this.port = port;
+ 		this.ssl = new Ssl();
+ 		this.compression = new Compression();
+ 	}
 
 	/**
 	 * The port that the web server listens on.
