52036f381937389051c1cd6cdcf426ab77e4e994
Pascal-Joos <pascal.joos@outlook.de>
2025-12-17 13:13:35 +0000

    fix: Type='DEREFERENCE_NULLABLE', message='dereferenced expression loggingSystem is @Nullable' /home/vscode/nullness-benchmarks/spring-boot/spring-boot-project/spring-boot/src/main/java/org/springframework/boot/context/logging/LoggingApplicationListener.java:429 			Runnable shutdownHandler = loggingSystem.getShutdownHandler();

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/context/logging/LoggingApplicationListener.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/context/logging/LoggingApplicationListener.java
@@ -432,13 +432,16 @@ public class LoggingApplicationListener implements GenericApplicationListener {
    }
 
 	private void registerShutdownHookIfNecessary(Environment environment, @Nullable LoggingSystem loggingSystem) {
-		if (environment.getProperty(REGISTER_SHUTDOWN_HOOK_PROPERTY, Boolean.class, true)) {
-			Runnable shutdownHandler = loggingSystem.getShutdownHandler();
-			if (shutdownHandler != null && shutdownHookRegistered.compareAndSet(false, true)) {
-				registerShutdownHook(shutdownHandler);
-			}
-		}
-	}
+ 		if (loggingSystem == null) {
+ 			return;
+ 		}
+ 		if (environment.getProperty(REGISTER_SHUTDOWN_HOOK_PROPERTY, Boolean.class, true)) {
+ 			Runnable shutdownHandler = loggingSystem.getShutdownHandler();
+ 			if (shutdownHandler != null && shutdownHookRegistered.compareAndSet(false, true)) {
+ 				registerShutdownHook(shutdownHandler);
+ 			}
+ 		}
+ 	}
 
 	void registerShutdownHook(Runnable shutdownHandler) {
 		SpringApplication.getShutdownHandlers().add(shutdownHandler);
