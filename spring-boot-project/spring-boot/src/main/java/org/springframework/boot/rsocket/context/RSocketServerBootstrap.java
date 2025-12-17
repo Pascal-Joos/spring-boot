f2cadd4b644a44745ec8d3174aa1b6b091ca0e37
Pascal-Joos <pascal.joos@outlook.de>
2025-12-17 13:36:41 +0000

    fix: Type='DEREFERENCE_NULLABLE', message='dereferenced expression this.eventPublisher is @Nullable' /home/vscode/nullness-benchmarks/spring-boot/spring-boot-project/spring-boot/src/main/java/org/springframework/boot/rsocket/context/RSocketServerBootstrap.java:55 		this.eventPublisher.publishEvent(new RSocketServerInitializedEvent(this.server));

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/rsocket/context/RSocketServerBootstrap.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/rsocket/context/RSocketServerBootstrap.java
@@ -50,10 +50,13 @@ public class RSocketServerBootstrap implements ApplicationEventPublisherAware, S
 	}
 
 	@Override
-	public void start() {
-		this.server.start();
-		this.eventPublisher.publishEvent(new RSocketServerInitializedEvent(this.server));
-	}
+ 	public void start() {
+ 		this.server.start();
+ 		ApplicationEventPublisher publisher = this.eventPublisher;
+ 		if (publisher != null) {
+ 			publisher.publishEvent(new RSocketServerInitializedEvent(this.server));
+ 		}
+ 	}
 
 	@Override
 	public void stop() {
