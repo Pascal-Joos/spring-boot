b316ff01b96f3d55e31c1c4b270c06e155483539
Pascal-Joos <pascal.joos@outlook.de>
2026-01-08 20:17:10 +0000

    fix: 76 - DEREFERENCE_NULLABLE - return (Collection) this.beanFactory.getBeansOfType(WebServerFactoryCustomizer.class, false, false).values(); - dereferenced expression this.beanFactory is @Nullable

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/web/server/WebServerFactoryCustomizerBeanPostProcessor.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/web/server/WebServerFactoryCustomizerBeanPostProcessor.java
@@ -42,8 +42,8 @@ import javax.annotation.Nullable;
  */
 public class WebServerFactoryCustomizerBeanPostProcessor implements BeanPostProcessor, BeanFactoryAware {
 
-	@Nullable
-	private ListableBeanFactory beanFactory;
+	
+	@Nullable private ListableBeanFactory beanFactory;
 
 	@Nullable
 	private List<WebServerFactoryCustomizer<?>> customizers;
@@ -86,8 +86,9 @@ public class WebServerFactoryCustomizerBeanPostProcessor implements BeanPostProc
 	}
 
 	@SuppressWarnings({ "unchecked", "rawtypes" })
-	private Collection<WebServerFactoryCustomizer<?>> getWebServerFactoryCustomizerBeans() {
-		return (Collection) this.beanFactory.getBeansOfType(WebServerFactoryCustomizer.class, false, false).values();
-	}
+ 	private Collection<WebServerFactoryCustomizer<?>> getWebServerFactoryCustomizerBeans() {
+ 		Assert.state(this.beanFactory != null, "No BeanFactory available");
+ 		return (Collection) this.beanFactory.getBeansOfType(WebServerFactoryCustomizer.class, false, false).values();
+ 	}
 
 }
