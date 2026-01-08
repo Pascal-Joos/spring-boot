24884d4b820fe2c0042dab236f74d14fee78c73c
Pascal-Joos <pascal.joos@outlook.de>
2026-01-08 14:37:35 +0000

    fix: 1 - DEREFERENCE_NULLABLE - ((GroovyBeanDefinitionReader) this.groovyReader).beans(loader.getBeans()); - dereferenced expression ((GroovyBeanDefinitionReader) this.groovyReader) is @Nullable

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/BeanDefinitionLoader.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/BeanDefinitionLoader.java
@@ -69,8 +69,8 @@ class BeanDefinitionLoader {
 
 	private final AbstractBeanDefinitionReader xmlReader;
 
-	@Nullable
-	private final BeanDefinitionReader groovyReader;
+	
+	@Nullable private final BeanDefinitionReader groovyReader;
 
 	private final ClassPathBeanDefinitionScanner scanner;
 
@@ -161,15 +161,17 @@ class BeanDefinitionLoader {
 	}
 
 	private void load(Class<?> source) {
-		if (isGroovyPresent() && GroovyBeanDefinitionSource.class.isAssignableFrom(source)) {
-			// Any GroovyLoaders added in beans{} DSL can contribute beans here
-			GroovyBeanDefinitionSource loader = BeanUtils.instantiateClass(source, GroovyBeanDefinitionSource.class);
-			((GroovyBeanDefinitionReader) this.groovyReader).beans(loader.getBeans());
-		}
-		if (isEligible(source)) {
-			this.annotatedReader.register(source);
-		}
-	}
+ 		if (isGroovyPresent() && GroovyBeanDefinitionSource.class.isAssignableFrom(source)) {
+ 			if (this.groovyReader == null) {
+ 				throw new BeanDefinitionStoreException("Cannot load Groovy beans without Groovy on classpath");
+ 			}
+ 			GroovyBeanDefinitionSource loader = BeanUtils.instantiateClass(source, GroovyBeanDefinitionSource.class);
+ 			((GroovyBeanDefinitionReader) this.groovyReader).beans(loader.getBeans());
+ 		}
+ 		if (isEligible(source)) {
+ 			this.annotatedReader.register(source);
+ 		}
+   }
 
 	private void load(Resource source) {
 		if (source.getFilename().endsWith(".groovy")) {
