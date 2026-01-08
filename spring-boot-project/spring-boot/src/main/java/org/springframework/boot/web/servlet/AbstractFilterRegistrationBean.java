c345270896672f407ec133b9198956746efe8900
Pascal-Joos <pascal.joos@outlook.de>
2026-01-08 20:21:16 +0000

    Revert "fix: 77 - DEREFERENCE_NULLABLE - filter.getClass().getClassLoader()) && filter instanceof OncePerRequestFilter) { - dereferenced expression filter is @Nullable"
    This reverts commit 3d685531b15cf0670cf9687d1caa116df4a80577.

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/web/servlet/AbstractFilterRegistrationBean.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/web/servlet/AbstractFilterRegistrationBean.java
@@ -218,46 +218,45 @@ public abstract class AbstractFilterRegistrationBean<T extends Filter> extends D
 	 * @param registration the registration
 	 */
 	@Override
- 	protected void configure(FilterRegistration.Dynamic registration) {
- 		super.configure(registration);
- 		EnumSet<DispatcherType> dispatcherTypes = this.dispatcherTypes;
- 		if (dispatcherTypes == null) {
- 			T filter = getFilter();
- 			Assert.notNull(filter, "Filter must not be null");
- 			if (ClassUtils.isPresent("org.springframework.web.filter.OncePerRequestFilter",
- 					filter.getClass().getClassLoader()) && filter instanceof OncePerRequestFilter) {
- 				dispatcherTypes = EnumSet.allOf(DispatcherType.class);
- 			}
- 			else {
- 				dispatcherTypes = EnumSet.of(DispatcherType.REQUEST);
- 			}
- 		}
- 		Set<String> servletNames = new LinkedHashSet<>();
- 		for (ServletRegistrationBean<?> servletRegistrationBean : this.servletRegistrationBeans) {
- 			servletNames.add(servletRegistrationBean.getServletName());
- 		}
- 		servletNames.addAll(this.servletNames);
- 		if (servletNames.isEmpty() && this.urlPatterns.isEmpty()) {
- 			registration.addMappingForUrlPatterns(dispatcherTypes, this.matchAfter, DEFAULT_URL_MAPPINGS);
- 		}
- 		else {
- 			if (!servletNames.isEmpty()) {
- 				registration.addMappingForServletNames(dispatcherTypes, this.matchAfter,
- 						StringUtils.toStringArray(servletNames));
- 			}
- 			if (!this.urlPatterns.isEmpty()) {
- 				registration.addMappingForUrlPatterns(dispatcherTypes, this.matchAfter,
- 						StringUtils.toStringArray(this.urlPatterns));
- 			}
- 		}
- }
+	protected void configure(FilterRegistration.Dynamic registration) {
+		super.configure(registration);
+		EnumSet<DispatcherType> dispatcherTypes = this.dispatcherTypes;
+		if (dispatcherTypes == null) {
+			T filter = getFilter();
+			if (ClassUtils.isPresent("org.springframework.web.filter.OncePerRequestFilter",
+					filter.getClass().getClassLoader()) && filter instanceof OncePerRequestFilter) {
+				dispatcherTypes = EnumSet.allOf(DispatcherType.class);
+			}
+			else {
+				dispatcherTypes = EnumSet.of(DispatcherType.REQUEST);
+			}
+		}
+		Set<String> servletNames = new LinkedHashSet<>();
+		for (ServletRegistrationBean<?> servletRegistrationBean : this.servletRegistrationBeans) {
+			servletNames.add(servletRegistrationBean.getServletName());
+		}
+		servletNames.addAll(this.servletNames);
+		if (servletNames.isEmpty() && this.urlPatterns.isEmpty()) {
+			registration.addMappingForUrlPatterns(dispatcherTypes, this.matchAfter, DEFAULT_URL_MAPPINGS);
+		}
+		else {
+			if (!servletNames.isEmpty()) {
+				registration.addMappingForServletNames(dispatcherTypes, this.matchAfter,
+						StringUtils.toStringArray(servletNames));
+			}
+			if (!this.urlPatterns.isEmpty()) {
+				registration.addMappingForUrlPatterns(dispatcherTypes, this.matchAfter,
+						StringUtils.toStringArray(this.urlPatterns));
+			}
+		}
+	}
 
 	/**
 	 * Return the {@link Filter} to be registered.
 	 * @return the filter
 	 */
-	
-	@Nullable public abstract T getFilter();
+	@Nullable
+	public abstract T getFilter();
 
 	@Override
 	public String toString() {
