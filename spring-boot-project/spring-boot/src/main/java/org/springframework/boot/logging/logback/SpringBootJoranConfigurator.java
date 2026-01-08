fb8d6019983b151d38782f2013360a4588c14085
Pascal-Joos <pascal.joos@outlook.de>
2026-01-08 18:40:01 +0000

    Revert "fix: 56 - DEREFERENCE_NULLABLE - return type.getConstructor().newInstance(); - dereferenced expression type is @Nullable"
    This reverts commit b0cd341634eb2eff0581fa9cf608d7c0edb56446.

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/logging/logback/SpringBootJoranConfigurator.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/logging/logback/SpringBootJoranConfigurator.java
@@ -278,17 +278,14 @@ class SpringBootJoranConfigurator extends JoranConfigurator {
 		}
 
 		@Nullable
-  		private Object instantiate( @Nullable Class<?> type) {
-  			if (type == null) {
-  				return null;
-  			}
-  			try {
-  				return type.getConstructor().newInstance();
-  			}
-  			catch (Exception ex) {
-  				return null;
-  			}
-  }
+		private Object instantiate(@Nullable Class<?> type) {
+			try {
+				return type.getConstructor().newInstance();
+			}
+			catch (Exception ex) {
+				return null;
+			}
+		}
 
 		private void processComponent(Class<?> componentType, Set<String> reflectionTypes) {
 			BeanDescription beanDescription = this.modelInterpretationContext.getBeanDescriptionCache()
