7ad043c4362e13d95175155f03c8256ee9540484
Pascal-Joos <pascal.joos@outlook.de>
2026-01-08 18:04:29 +0000

    fix: 48 - PASS_NULLABLE - this.setter.set(dataSource, convertFromString(value)); - passing @Nullable parameter 'convertFromString(value)' where @NonNull is required

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/jdbc/DataSourceBuilder.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/jdbc/DataSourceBuilder.java
@@ -46,6 +46,7 @@ import org.springframework.util.ClassUtils;
 import org.springframework.util.ReflectionUtils;
 import org.springframework.util.StringUtils;
 import javax.annotation.Nullable;
+import edu.ucr.cs.riple.annotator.util.Nullability;
 
 /**
  * Convenience class for building a {@link DataSource}. Provides a limited subset of the
@@ -492,19 +493,19 @@ public final class DataSourceBuilder<T extends DataSource> {
 			this.setter = setter;
 		}
 
-		void set(T dataSource, @Nullable String value) {
-			try {
-				if (this.setter == null) {
-					UnsupportedDataSourcePropertyException.throwIf(!this.property.isOptional(),
-							() -> "No setter mapped for '" + this.property + "' property");
-					return;
-				}
-				this.setter.set(dataSource, convertFromString(value));
-			}
-			catch (SQLException ex) {
-				throw new IllegalStateException(ex);
-			}
-		}
+		void set(T dataSource,  @Nullable String value) {
+  			try {
+  				if (this.setter == null) {
+  					UnsupportedDataSourcePropertyException.throwIf(!this.property.isOptional(),
+  							() -> "No setter mapped for '" + this.property + "' property");
+  					return;
+  				}
+  				this.setter.set(dataSource, Nullability.castToNonnull(convertFromString(value)));
+  			}
+  			catch (SQLException ex) {
+  				throw new IllegalStateException(ex);
+  			}
+  }
 
 		@Nullable
 		String get(@Nullable T dataSource) {
