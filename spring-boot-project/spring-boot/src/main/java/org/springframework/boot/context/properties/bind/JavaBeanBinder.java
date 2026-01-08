77806fa2d0272c04a55d569febbf5ac04e4ac427
Pascal-Joos <pascal.joos@outlook.de>
2026-01-08 16:58:48 +0000

    fix: 33 - METHOD_NO_INIT - BeanProperty(String name, ResolvableType declaringClassType) { - initializer method does not guarantee @NonNull fields getter (line 289), setter (line 291) are initialized along all control-flow paths (remember to check for exceptions or early returns).

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/context/properties/bind/JavaBeanBinder.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/context/properties/bind/JavaBeanBinder.java
@@ -286,9 +286,9 @@ class JavaBeanBinder implements DataObjectBinder {
 
 		private final ResolvableType declaringClassType;
 
-		private Method getter;
+		@Nullable private Method getter;
 
-		private Method setter;
+		@Nullable private Method setter;
 
 		@Nullable
 		private Field field;
@@ -348,34 +348,40 @@ class JavaBeanBinder implements DataObjectBinder {
 		}
 
 		@Nullable
-		Supplier<Object> getValue(Supplier<?> instance) {
-			if (this.getter == null) {
-				return null;
-			}
-			return () -> {
-				try {
-					this.getter.setAccessible(true);
-					return this.getter.invoke(instance.get());
-				}
-				catch (Exception ex) {
-					throw new IllegalStateException("Unable to get value for property " + this.name, ex);
-				}
-			};
-		}
+  		Supplier<Object> getValue(Supplier<?> instance) {
+  			if (this.getter == null) {
+  				return null;
+  			}
+  			return () -> {
+  				try {
+  					if (this.getter == null) {
+  						return null;
+  					}
+  					this.getter.setAccessible(true);
+  					return this.getter.invoke(instance.get());
+  				}
+  				catch (Exception ex) {
+  					throw new IllegalStateException("Unable to get value for property " + this.name, ex);
+  				}
+  			};
+  }
 
 		boolean isSettable() {
 			return this.setter != null;
 		}
 
 		void setValue(Supplier<?> instance, Object value) {
-			try {
-				this.setter.setAccessible(true);
-				this.setter.invoke(instance.get(), value);
-			}
-			catch (Exception ex) {
-				throw new IllegalStateException("Unable to set value for property " + this.name, ex);
-			}
-		}
+  			if (!isSettable()) {
+  				throw new IllegalStateException("No setter available for property " + this.name);
+  			}
+  			try {
+  				this.setter.setAccessible(true);
+  				this.setter.invoke(instance.get(), value);
+  			}
+  			catch (Exception ex) {
+  				throw new IllegalStateException("Unable to set value for property " + this.name, ex);
+  			}
+    }
 
 	}
 
