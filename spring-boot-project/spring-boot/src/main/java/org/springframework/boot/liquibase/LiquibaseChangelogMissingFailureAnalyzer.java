306ed6f13bde2ddd9ce21f5e83ea34f8a1719df8
Pascal-Joos <pascal.joos@outlook.de>
2026-01-08 18:14:53 +0000

    fix: 50 - DEREFERENCE_NULLABLE - if (cause.getMessage().endsWith(MESSAGE_SUFFIX)) { - dereferenced expression cause.getMessage() is @Nullable

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/liquibase/LiquibaseChangelogMissingFailureAnalyzer.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/liquibase/LiquibaseChangelogMissingFailureAnalyzer.java
@@ -32,16 +32,20 @@ class LiquibaseChangelogMissingFailureAnalyzer extends AbstractFailureAnalyzer<C
 
 	private static final String MESSAGE_SUFFIX = " does not exist";
 
-	@Nullable
 	@Override
-	protected FailureAnalysis analyze(Throwable rootFailure, ChangeLogParseException cause) {
-		if (cause.getMessage().endsWith(MESSAGE_SUFFIX)) {
-			String changelogPath = extractChangelogPath(cause);
-			return new FailureAnalysis(getDescription(changelogPath),
-					"Make sure a Liquibase changelog is present at the configured path.", cause);
-		}
-		return null;
-	}
+ 	@Nullable
+ 	protected FailureAnalysis analyze(Throwable rootFailure, ChangeLogParseException cause) {
+ 		String message = cause.getMessage();
+ 		if (message == null) {
+ 			return null;
+ 		}
+ 		if (message.endsWith(MESSAGE_SUFFIX)) {
+ 			String changelogPath = extractChangelogPath(cause);
+ 			return new FailureAnalysis(getDescription(changelogPath),
+ 					"Make sure a Liquibase changelog is present at the configured path.", cause);
+ 		}
+ 		return null;
+ }
 
 	private String extractChangelogPath(ChangeLogParseException cause) {
 		return cause.getMessage().substring(0, cause.getMessage().length() - MESSAGE_SUFFIX.length());
