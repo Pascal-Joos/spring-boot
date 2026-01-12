6d035a26452d5e4ad1ec0dbf92fcbae8ecae5b41
Pascal-Joos <pascal.joos@outlook.de>
2026-01-12 08:53:32 +0000

    fix: Type='DEREFERENCE_NULLABLE', message='dereferenced expression this.bindConstructor is @Nullable' /home/vscode/nullness-benchmarks/spring-boot/spring-boot-project/spring-boot/src/main/java/org/springframework/boot/context/properties/bind/BindableRuntimeHintsRegistrar.java:154 			for (int i = 0; i < this.bindConstructor.getParameterCount(); i++) {

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/context/properties/bind/BindableRuntimeHintsRegistrar.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/context/properties/bind/BindableRuntimeHintsRegistrar.java
@@ -90,8 +90,8 @@ public class BindableRuntimeHintsRegistrar implements RuntimeHintsRegistrar {
 
 		private final Class<?> type;
 
-		@Nullable
-		private final Constructor<?> bindConstructor;
+		
+		@Nullable private final Constructor<?> bindConstructor;
 
 		
 		@Nullable private final BeanInfo beanInfo;
@@ -151,12 +151,15 @@ public class BindableRuntimeHintsRegistrar implements RuntimeHintsRegistrar {
 		}
 
 		private void handleValueObjectProperties(ReflectionHints hints) {
-			for (int i = 0; i < this.bindConstructor.getParameterCount(); i++) {
-				String propertyName = this.bindConstructor.getParameters()[i].getName();
-				ResolvableType propertyType = ResolvableType.forConstructorParameter(this.bindConstructor, i);
-				handleProperty(hints, propertyName, propertyType);
-			}
-		}
+  			if (this.bindConstructor == null) {
+  				return;
+  			}
+  			for (int i = 0; i < this.bindConstructor.getParameterCount(); i++) {
+  				String propertyName = this.bindConstructor.getParameters()[i].getName();
+  				ResolvableType propertyType = ResolvableType.forConstructorParameter(this.bindConstructor, i);
+  				handleProperty(hints, propertyName, propertyType);
+  			}
+  }
 
 		private void handleJavaBeanProperties(ReflectionHints hints) {
   			if (this.beanInfo == null) {
