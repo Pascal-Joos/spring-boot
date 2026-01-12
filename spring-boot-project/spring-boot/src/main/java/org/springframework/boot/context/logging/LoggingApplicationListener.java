7523ae58aa584fb26e3376a9615b5498a225d1e0
Pascal-Joos <pascal.joos@outlook.de>
2026-01-12 08:42:55 +0000

    fix: Type='DEREFERENCE_NULLABLE', message='dereferenced expression this.loggingSystem is @Nullable' /home/vscode/nullness-benchmarks/spring-boot/spring-boot-project/spring-boot/src/main/java/org/springframework/boot/context/logging/LoggingApplicationListener.java:243 		this.loggingSystem.beforeInitialize();

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/context/logging/LoggingApplicationListener.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/context/logging/LoggingApplicationListener.java
@@ -240,9 +240,9 @@ public class LoggingApplicationListener implements GenericApplicationListener {
 	}
 
 	private void onApplicationStartingEvent(ApplicationStartingEvent event) {
-		this.loggingSystem = LoggingSystem.get(event.getSpringApplication().getClassLoader());
-		this.loggingSystem.beforeInitialize();
-	}
+ 		this.loggingSystem = LoggingSystem.get(event.getSpringApplication().getClassLoader());
+ 		Nullability.castToNonnull(this.loggingSystem).beforeInitialize();
+ }
 
 	private void onApplicationEnvironmentPreparedEvent(ApplicationEnvironmentPreparedEvent event) {
 		SpringApplication springApplication = event.getSpringApplication();
